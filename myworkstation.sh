3579234fad85e6f0fc3b603517ce1ce3338b2c97
nilberthsouza <58176211+nilberthsouza@users.noreply.github.com>
2020-05-17 12:58:51 -0300

    Update myworkstation.sh

--- myworkstation.sh
+++ myworkstation.sh
@@ -4,8 +4,11 @@ sudo apt update &&
 
 sudo apt install python3 python-pip wine nautilus-dropbox git build-essential libssl-dev flatpak -y &&
 
+
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
 
 sudo apt install tmux &&
+
+sudo apt install pychess &&
 
 echo "finalizado"
