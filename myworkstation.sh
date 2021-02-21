b49ed0a224587aff8fc191c7a381701817bf3497
nilberthsouza <58176211+nilberthsouza@users.noreply.github.com>
2021-02-21 01:42:22 -0300

    apt-get

--- myworkstation.sh
+++ myworkstation.sh
@@ -7,21 +7,21 @@ sudo apt install python3 python-pip wine nautilus-dropbox git build-essential li
 
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
 
-sudo apt install tmux -y &&
+sudo apt-get install tmux -y &&
 
-sudo apt install pychess -y&&
+sudo apt-get install pychess -y&&
 
-sudo apt install visualboyadvance -y &&
+sudo apt-get install visualboyadvance -y &&
 
-sudo apt install git -y &&
+sudo apt-get install git -y &&
 
-sudo apt install mednaffe &&
+sudo apt-get install mednaffe &&
 
-sudo apt install zsnes -y &&
+sudo apt-get install zsnes -y &&
 
-sudo apt install pcsxr -y &&
+sudo apt-get install pcsxr -y &&
 
-sudo apt install xfce4-terminal &&
+sudo apt-get install xfce4-terminal &&
 
 sudo apt-get install simplescreenrecorder &&
 
