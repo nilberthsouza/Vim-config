8e2c630e2920f29f16eff299c344ef96e268d91b
nilberthsouza <58176211+nilberthsouza@users.noreply.github.com>
2022-12-03 23:43:15 -0300

    Update myworkstation.sh

--- myworkstation.sh
+++ myworkstation.sh
@@ -4,7 +4,6 @@ sudo apt update &&
 
 sudo apt install python3 python-pip wine nautilus-dropbox git build-essential libssl-dev flatpak -y &&
 
-
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
 
 sudo apt-get install tmux -y &&
